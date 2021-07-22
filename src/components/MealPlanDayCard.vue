3b05a58efcbf29179c8f5edc75e5b929df9f7db1
nhoward712 <Nhoward72@yahoo.com>
2021-07-22 13:27:09 -0500

    Made recipes on meal planner page links to recipes

--- src/components/MealPlanDayCard.vue
+++ src/components/MealPlanDayCard.vue
@@ -3,20 +3,21 @@
         <div class="card border-primary m-1 "  style="width: 12rem;">
             <h3 class="bg-secondary card-body">{{DayOfWeek}}</h3>
             <div v-for="per in period" :key="per.period">
+
                 <div class="row">
                     <h5 class="mt-3 ml-3">{{per}}:</h5>
                     <router-link class="mt-2 col-sm-5 " style="font-size: .5em" :to="{name:'AddToPlan', params: { day: DayOfWeek, period: per }}" >
                         <button class="btn fa-border">
                             <font-awesome-icon icon="plus-circle" />
-
                         </button>
                     </router-link>
                 </div>
+
                 <div v-for="(recipe) in Recipes" :key="recipe.Recipes">
                     <p v-if="DayOfWeek === recipe.day && recipe.mealPeriod === per" class="ml-4">
                         <button class="btn fa-border" style="font-size: .8em" v-on:click="remove(recipe,per,1)" >
                             <font-awesome-icon icon="minus-circle" />
-                        </button> {{recipe.recipeName}}</p>
+                        </button><router-link class="mt-0 col-sm-5" :to="{name:'ViewRecipe', params: {recipe:recipe}}" > {{recipe.recipeName}}</router-link></p>
                 </div>
             </div>
         </div>
